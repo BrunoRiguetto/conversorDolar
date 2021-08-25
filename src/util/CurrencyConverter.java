d7d05d865914b09e7bcff096e260d91c255a0d9d
BrunoRiguetto <bruno.riguetto@gmail.com>
2021-08-25 15:21:50 -0300

    Projeto criado

---
+++ src/util/CurrencyConverter.java
@@ -0,0 +1,16 @@
+package util;
+
+public class CurrencyConverter {
+	
+	public static double dollarPrice;
+	
+	public static double quantity;
+	
+	public static double reais;
+	
+	
+	public static double converter() {
+		return reais = dollarPrice * 1.06 * quantity;
+	}
+
+}
